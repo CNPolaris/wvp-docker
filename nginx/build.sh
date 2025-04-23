c67bac40cb58182d539440826d9f52afccf38b8c
polaris <1875091912@qq.com>
2025-04-23 20:09:57 +0800

    修订

--- nginx/build.sh
+++ nginx/build.sh
@@ -3,6 +3,9 @@ set -e
 
 version=2.7.3
 
+rm ./dist/static/js/config.js
+cp ./config.js ./dist/static/js/
+
 docker build -t ylcx-nginx:${version} .
 docker tag ylcx-nginx:${version} polaris-tian-docker.pkg.coding.net/qt/polaris/ylcx-nginx:${version}
 docker tag ylcx-nginx:${version} polaris-tian-docker.pkg.coding.net/qt/polaris/ylcx-nginx:latest
