e21c819ff7610063cc74f06bb460bffd9afe8468
polaris <1875091912@qq.com>
2025-04-30 21:41:38 +0800

    fix

--- push.sh
+++ push.sh
@@ -2,7 +2,6 @@
 set -e
 
 version=2.7.3
-docker login -u polaris-1745395195527 -p 32115838d22d034cb3d9e9fcdcc607c5b5d55b90 polaris-tian-docker.pkg.coding.net
 
 docker push polaris-tian-docker.pkg.coding.net/qt/polaris/polaris-media:latest
 docker push polaris-tian-docker.pkg.coding.net/qt/polaris/polaris-mysql:latest
