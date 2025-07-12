84f7bd454a513784c8340d77d637d424f4cdd504
polaris <1875091912@qq.com>
2025-07-12 22:45:33 +0800

    再次测试

--- redis/build.sh
+++ redis/build.sh
@@ -10,7 +10,7 @@ else
   arch=amd64
 fi
 
-docker build -t polaris-redis:${version} .
-docker tag polaris-redis:${version} docker.cnb.cool/polaris-tian/wvp-docker/polaris-redis:${version}
-docker tag polaris-redis:${version} docker.cnb.cool/polaris-tian/wvp-docker/polaris-redis:${arch}
-docker tag polaris-redis:${version} docker.cnb.cool/polaris-tian/wvp-docker/polaris-redis:latest
+docker build -t polaris-redis:${arch} .
+docker tag polaris-redis:${arch} docker.cnb.cool/polaris-tian/wvp-docker/polaris-redis:${version}
+docker tag polaris-redis:${arch} docker.cnb.cool/polaris-tian/wvp-docker/polaris-redis:${arch}
+docker tag polaris-redis:${arch} docker.cnb.cool/polaris-tian/wvp-docker/polaris-redis:latest
