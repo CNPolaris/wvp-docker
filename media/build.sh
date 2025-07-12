54d0f7fe8dfcc7c4a52050cfdbeac4053c8a911b
polaris <1875091912@qq.com>
2025-07-12 23:52:43 +0800

    fix media conf

--- media/build.sh
+++ media/build.sh
@@ -10,6 +10,6 @@ else
   arch=amd64
 fi
 
-docker build -t polaris-media:${version} .
+docker build --build-arg MODEL=Release -t polaris-media:${version} .
 docker tag polaris-media:${version} docker.cnb.cool/polaris-tian/wvp-docker/polaris-media:${version}-linux-${arch}
 docker tag polaris-media:${version} docker.cnb.cool/polaris-tian/wvp-docker/polaris-media:latest-linux-${arch}
