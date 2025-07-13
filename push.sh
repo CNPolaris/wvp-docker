21e05cd1f6bc04458c1aa1733d23cf30e4aa3132
polaris <1875091912@qq.com>
2025-07-13 14:07:10 +0800

    整体测试

--- push.sh
+++ push.sh
@@ -11,17 +11,17 @@ else
   arch=amd64
 fi
 
-# docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-redis:latest-linux-${arch}
+docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-redis:latest-linux-${arch}
 
 # docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-mysql:latest-linux-${arch}
-# docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-mysql:${version}-linux-${arch}
+docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-mysql:${version}-linux-${arch}
 
 # docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-media:latest-linux-${arch}
-# docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-media:${version}-linux-${arch}
+docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-media:${version}-linux-${arch}
 
 # docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-wvp:latest-linux-${arch}
 docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-wvp:${version}-linux-${arch}
 
 # docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-nginx:latest-linux-${arch}
-# docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-nginx:${version}-linux-${arch}
+docker push docker.cnb.cool/polaris-tian/wvp-docker/polaris-nginx:${version}-linux-${arch}
 
